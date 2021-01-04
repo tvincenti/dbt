f0885bf3763c1e134f51c77eabc54dfbcb5745a3
Tim Vincenti <tim.vincenti@outlook.com>
2021-01-04 22:07:15 +0000

    fixing dates

--- models/example/dates.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='incremental', unique_key = 'd_date') }}
-
-SELECT *
-FROM "SNOWFLAKE_SAMPLE_DATA"."TPCDS_SF100TCL"."DATE_DIM"
-WHERE d_date <= current_date
-
-{% if is_incremental() %}
-   and d_date > (select max(d_date) from {{this}}
-{% endif %}
