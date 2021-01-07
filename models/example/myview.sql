44df7da28d3a78988301d8b246da359851950644
Tim Vincenti <tim.vincenti@outlook.com>
2021-01-07 14:19:55 +0000

    custom schema

--- models/example/myview.sql
+++ models/example/myview.sql
@@ -1,8 +1,8 @@
-
+{{ config(schema='stripe', alias='payments', ...) }}
 
 {% macro generate_schema_name(custom_schema_name, node) -%}
   {%- set default_schema = target.schema -%}
-  {%- set custom_schema_name = 'abc' -%}
+ 
   {%- if custom_schema_name is none -%}
     {{ default_schema }}
   {%- else -%}
