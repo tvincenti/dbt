badcc6d76e7787a68d090aced522d8baf9211c44
Tim Vincenti <tim.vincenti@outlook.com>
2021-01-07 14:06:22 +0000

    custom schema

--- models/example/myview.sql
+++ models/example/myview.sql
@@ -1,2 +1,11 @@
-{{ config(schema='EXISTING_MODELS') }}
+{{ config(schema='test') }}
+
+{% macro generate_schema_name(custom_schema_name, node) -%}
+  {%- set default_schema = target.schema -%}
+  {%- if custom_schema_name is none -%}
+    {{ default_schema }}
+  {%- else -%}
+    {{ custom_schema_name | trim }}
+  {%- endif -%}
+{%- endmacro %}
 SELECT number, 'sales' as sales FROM dbt_EXISTING_STAGING.myview_stg
