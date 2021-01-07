f6e01313feab2f708e2873f953752a992b20f812
Tim Vincenti <tim.vincenti@outlook.com>
2021-01-07 14:25:45 +0000

    cleaning up myview

--- models/example/myview.sql
+++ models/example/myview.sql
@@ -1,15 +1,3 @@
 {{ config(schema='stripe', alias='payments') }}
 
-{% macro generate_schema_name(custom_schema_name, node) -%}
-  {%- set default_schema = target.schema -%}
- 
-  {%- if custom_schema_name is none -%}
-    {{ default_schema }}
-  {%- else -%}
-    {{ custom_schema_name | trim }}
-  {%- endif -%}
-{%- endmacro %}
-
-
-
 SELECT number, 'sales' as sales FROM dbt_EXISTING_STAGING.myview_stg
